4229fa932837be2d8eca42f187880d9b1852d5dd
HyunJun Kim <HyunJun Kim>
2016-07-20 04:34:10 +0900

    Initialize project with libGDX setup

---
+++ desktop/src/com/eigenic/salix/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.eigenic.salix.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.eigenic.salix.SalixGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SalixGame(), config);
+	}
+}
---
+++ ios/src/com/eigenic/salix/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.eigenic.salix;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.eigenic.salix.SalixGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new SalixGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
