74f30db77b38d48a4b158f6feb0678584832f169
HyunJun Kim <HyunJun Kim>
2016-07-21 01:47:17 +0900

    Add SxWorldScreen class

--- core/src/com/eigenic/salix/screens/SxSigninScreen.java
+++ core/src/com/eigenic/salix/screens/SxSigninScreen.java
@@ -114,8 +114,6 @@ public class SxSigninScreen implements Screen, Input.TextInputListener {
 
                 textPrompt.build().show();
 
-
-
             }
         });
 
---
+++ core/src/com/eigenic/salix/screens/SxWorldScreen.java
@@ -0,0 +1,48 @@
+package com.eigenic.salix.screens;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by HyunJun on 2016-07-21.
+ */
+public class SxWorldScreen implements Screen {
+
+    public SxWorldScreen() {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
